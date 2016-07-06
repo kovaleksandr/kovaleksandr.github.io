854f00002f6907622b0d46c2171c778be7ff559a
Soulerik <soulerik@me.com>
2016-07-06 22:43:17 +0300

    added null checking

--- js1/script1.js
+++ js1/script1.js
@@ -1,7 +1,27 @@
 var number = prompt('Put in number');
 var power = prompt('Put in power');
+
 var result = 1;
-for (i=1; i <= power; i++) {
-  result *= number;
+if (power !== null && number !== null) {
+
+  switch (power >= 0) {
+      case true:
+        for (i=1; i <= power; i++) {
+          result *= number;
+          console.log(result);
+        }
+        break;
+
+        case false:
+        power = -power;
+        for (i=1; i <= power; i++) {
+          result /= number;
+          console.log(result);
+        }
+        break;
 }
 console.log(result);
+}
+else {
+  console.log('You should put in number');
+}
